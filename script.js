9d3799447fbe59b988625700e44c41acb98f1605
Ashu <ashutolosa3@gmail.com>
2020-11-24 16:52:50 +0300

    Variable Mutation and Type Coercion

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/*
 console.log("Hello World!!!");
 var hello = 'goof';
 console.log(hello);
@@ -7,8 +8,30 @@ console.log(hello +" "+ bye)
 var job;
 job = "hello";
 console.log(job);
+*/
 /*
 var james = 30;
 var fullAge = true;
 console.log(fullAge);
 */
+
+/**************   Variable Mutation and Type Coercion ***************/
+
+var firstName = "John";
+var age = 28;
+
+// Type Coercion
+
+var job, isMarried;
+job = "Teacher";
+isMarried = "false";
+console.log(firstName+" is a "+age+" years old "+ job+". Is he married?"+isMarried);
+
+// Variable Mutation
+
+age = 'twenty eight';
+job = 'driver';
+alert(firstName+" is a "+age+" years old "+ job+". Is he married?"+isMarried);
+
+var lastName = prompt("What is his last name ?");
+console.log(firstName + " " +lastName);
