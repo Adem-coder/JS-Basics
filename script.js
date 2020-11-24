1cf1219b3545f248d8f52f33ffab20dfe24fbe29
Ashu <ashutolosa3@gmail.com>
2020-11-24 15:00:05 +0300

    JavaScript language basics

---
+++ script.js
@@ -0,0 +1,14 @@
+console.log("Hello World!!!");
+var hello = 'goof';
+console.log(hello);
+var bye = 'ball';
+console.log(bye);
+console.log(hello +" "+ bye)
+var job;
+job = "hello";
+console.log(job);
+/*
+var james = 30;
+var fullAge = true;
+console.log(fullAge);
+*/
