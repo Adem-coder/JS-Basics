e423b7696263df7bd2f8899610ac4ddd2aa6aad8
Ashu <ashutolosa3@gmail.com>
2020-11-25 11:50:36 +0300

    Basic Operators

--- script.js
+++ script.js
@@ -16,12 +16,14 @@ console.log(fullAge);
 */
 
 /**************   Variable Mutation and Type Coercion ***************/
-
+/*
 var firstName = "John";
-var age = 28;
+var age = 28; 
+*/
 
 // Type Coercion
 
+/*
 var job, isMarried;
 job = "Teacher";
 isMarried = "false";
@@ -35,3 +37,26 @@ alert(firstName+" is a "+age+" years old "+ job+". Is he married?"+isMarried);
 
 var lastName = prompt("What is his last name ?");
 console.log(firstName + " " +lastName);
+*/
+
+/******************** Basic Operators *****************************/
+
+// Maths operators
+var now, ageJohn, ageJames;
+ageJohn = 28;
+ageJames = 33;
+now = 2020;
+yearJohn = now - 28;
+yearJames = now - 33;
+console.log(yearJohn);
+console.log(now + 2);
+
+//Logical Operators
+var older = ageJames > ageJohn;
+console.log(older);
+
+//Type of Operator
+console.log(typeof older);
+console.log(typeof ageJohn);
+ var x;
+ console.log(typeof x);
